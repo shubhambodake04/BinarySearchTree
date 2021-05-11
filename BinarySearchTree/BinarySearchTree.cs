5acdf9815a923d736bf8617d2d01405dc56716b5
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-11 21:24:00 +0530

    Add element in tree

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -4,7 +4,56 @@ using System.Text;
 
 namespace BinarySearchTree
 {
-    class BinarySearchTree
+    class BinarySearchTree<T> where T : IComparable<T>
     {
+        public T NodeData { get; set; }
+        public BinarySearchTree<T> leftTree { get; set; }
+        public BinarySearchTree<T> rightTree { get; set; }
+        public BinarySearchTree(T NodeData)
+        {
+            this.NodeData = NodeData;
+            this.leftTree = null;
+            this.rightTree = null;
+
+        }
+        int leftCount = 0, rightCount = 0;
+        bool result = false;
+
+        public void Insert(T item)
+        {
+            T currentNodeValue = this.NodeData;
+            if ((currentNodeValue.CompareTo(item)) > 0)
+            {
+                if (this.leftTree == null)
+                    this.leftTree = new BinarySearchTree<T>(item);
+                else
+                    this.leftTree.Insert(item);
+            }
+            else
+            {
+                if (this.rightTree == null)
+                    this.rightTree = new BinarySearchTree<T>(item);
+                else
+                    this.rightTree.Insert(item);
+            }
+        }
+
+
+        public void Display()
+        {
+            if (this.leftTree != null)
+            {
+                this.leftCount++;
+                this.leftTree.Display();
+
+            }
+            Console.WriteLine(this.NodeData.ToString());
+            if (this.rightTree != null)
+            {
+                this.rightCount++;
+                this.rightTree.Display();
+            }
+        }
+
     }
 }
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -6,7 +6,11 @@ namespace BinarySearchTree
     {
         static void Main(string[] args)
         {
-           
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Display();
+            Console.ReadKey();
         }
     }
 }
